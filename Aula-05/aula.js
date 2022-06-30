0f423314c36eea353b0bdeb0416665f4d8012f7e
LeoIOT <leonardo_pereira1989@hotmail.com>
2022-06-30 18:14:22 -0300

    Aula - 05 - Fim

--- Aula-05/aula.js
+++ Aula-05/aula.js
@@ -1,3 +1,29 @@
 //Aula 05
 
-//Aula 5.1 - 
+//Aula 5.1 - if/else
+
+//Aula 5.2 - if/else
+
+//Aula 5.3 - if/else
+
+//Aula 5.4 - if/else
+
+//Aula 5.5 - if/else
+
+//Aula 5.6 - if/else
+
+//Aula 5.7 - if/else if...
+
+//Aula 5.8 - switch/case/default
+
+//Aula 5.9 - switch/case
+
+//Aula 5.10 - Operador Ternário
+
+let r = "", n = 10, x = 25;
+
+r = n>50? "Verdadeiro!":"Falso!";
+
+console.log(r);
+
+
